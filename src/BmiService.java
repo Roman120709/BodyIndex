eb210b9e0d88d087781d0d84bfefb7cea363c78d
Roman120709 <urall76@yandex.ru>
2022-01-07 17:32:06 +0300

    Commit file out

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,7 +1,7 @@
 public class BmiService {
     public double calculate(double massa, double height) {
-        double bodymassindex;
-        bodymassindex = massa/(height*height);
-        return bodymassindex;
+        double bodyMassIndex;
+        bodyMassIndex = massa / (height * height);
+        return bodyMassIndex;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         BmiService service = new BmiService();
         int massa = 80;
         double height = 1.76;
-        double bodymassindex = service.calculate(massa, height);
-        System.out.println(bodymassindex);
+        double bodyMassIndex = service.calculate(massa, height);
+        System.out.println(bodyMassIndex);
     }
 }
